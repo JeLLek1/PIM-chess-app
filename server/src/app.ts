5662f95a31bf51df4013606f06ff2bee7b03b57a
JeLLek <jellek@wp.pl>
2021-10-11 16:17:45 +0200

    Docker

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 app.get('/', (req, res) => {
-  res.send('The sedulous hs nsaddsad!');
+  res.send('Test');
 });
 app.listen(port, () => {
   return console.log(`server is listening on ${port}`);
