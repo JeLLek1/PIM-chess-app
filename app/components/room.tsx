1c4f24e9332e677724b81e08230b089805e38740
Wakir <50971678+Wakir@users.noreply.github.com>
2021-10-31 22:40:55 +0100

    Room-screen-second commit

--- app/components/room.tsx
+++ app/components/room.tsx
@@ -1,26 +1,49 @@
 import React from "react";
-import { View, Text, StyleSheet, Touchable } from 'react-native';
+import { View, Text, StyleSheet, Button, Image } from 'react-native';
+
+//import "../assets/room-icon.png";
 
 const Room = (prop: {text: string}) => {
     return (
         <View style={styles.room}>
-            <Text style={styles.roomText}>{prop.text}</Text>
+            <View style={styles.roomLeft}>
+                <Image source={require("../assets/room-icon.png")} style={styles.icon}/>
+                <Text style={styles.roomText}>{prop.text}</Text>
+            </View>
+            <View style={styles.button}>
+            <Button
+                title="Dołącz"
+                onPress={() => console.log('Dołączono')}
+                />
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    icon:{
+        width: 60, 
+        height: 60
+    },
+    roomLeft:{
+        flexDirection: 'row',
+        alignItems: "center",
+        flexWrap: 'wrap'
+    },
     room:{
         backgroundColor: "#FFF",
         padding: 15,
         borderRadius: 10,
         flexDirection: 'row',
         alignItems: "center",
-        justifyContent: "center",
-        marginBottom: 20,
+        justifyContent: "space-between",
+        marginBottom: 20
     },
     roomText:{
         maxWidth: '80%',
+    },
+    button:{
+
     }
 })
 
