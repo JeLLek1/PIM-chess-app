ba4e9e5a04a8fdd65a729de89588edd74436c32d
Wakir <50971678+Wakir@users.noreply.github.com>
2021-11-16 12:48:54 +0100

    Simple dialog modals + useState on RoomList

--- app/components/room.tsx
+++ app/components/room.tsx
@@ -1,23 +1,49 @@
-import React from "react";
-import { View, Text, StyleSheet, Button, Image } from 'react-native';
+import React, {useState} from "react";
+import { View, Text, StyleSheet, Button, Image, Modal, TextInput} from 'react-native';
 
 //import "../assets/room-icon.png";
 
-const Room = (prop: {text: string}) => {
+const Room = (prop: {name: string}) => {
+    const [ifVisible, setIsVisible] = useState(false)
+    const [nick, setNick] = useState()
+
     return (
+        <>
         <View style={styles.room}>
             <View style={styles.roomLeft}>
                 <Image source={require("../assets/room-icon.png")} style={styles.icon}/>
-                <Text style={styles.roomText}>{prop.text}</Text>
+                <Text style={styles.roomText}>{prop.name}</Text>
             </View>
             <View style={styles.button}>
             <Button
                 title="Dołącz"
-                onPress={() => console.log('Dołączono')}
+                onPress={() => setIsVisible(true)}
                 />
             </View>
         </View>
+        <JoinRoomModal/>
+        </>
     )
+    function JoinRoomModal(){
+        return(
+          <View style={styles.modal}>
+            <Modal visible={ifVisible}>
+              <View>
+                <TextInput value={nick} style={styles.input} placeholder="Nick"/>
+                <View style={styles.complete}>
+                <Button
+                  title="Dołącz"
+                   onPress={() => joinRoom()}
+                  />
+                </View>
+              </View>
+            </Modal>
+          </View>
+        )
+      }
+    function joinRoom(){
+        setIsVisible(false)
+    }
 }
 
 const styles = StyleSheet.create({
@@ -39,6 +65,31 @@ const styles = StyleSheet.create({
         justifyContent: "space-between",
         marginBottom: 20
     },
+    input:{
+        position: "relative",
+        justifyContent: "space-between",
+        allignItems: "center",
+        paddingVertical: 15,
+        paddingHorizontal: 15,
+        backgroundColor: '#FFF',
+        borderRadius: 60,
+        borderColor: "#C0C0C0",
+        borderWidth: 1,
+        width: '50%',
+      },
+      complete:{
+        position: 'relative',
+        allignItems: 'center',
+        width:"30%"
+      },
+    modal:{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        position: 'relative',
+        width:"40%",
+        height:"40%"
+      },
     roomText:{
         maxWidth: '80%',
     },
